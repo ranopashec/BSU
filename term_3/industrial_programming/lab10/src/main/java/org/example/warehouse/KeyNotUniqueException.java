1c0e06f936daa6308693ada9269536df81233293
ranopashec <semenpetrakov@gmail.com>
2025-05-19 11:08:34 +0300

    Add .vs and *.ipch to .gitignore

--- term_3/industrial_programming/lab10/src/main/java/org/example/warehouse/KeyNotUniqueException.java
+++
@@ -1,9 +0,0 @@
-package org.example.warehouse;
-
-public class KeyNotUniqueException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public KeyNotUniqueException(String key) {
-        super("Key is not unique: " + key);
-    }
-}
--- term_4/OS/Colloquium/include/palindrome.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Simon on 25/03/2025.
-//
-
-#ifndef PALINDROME_H
-#define PALINDROME_H
-bool isPalindrome(int number);
-#endif //PALINDROME_H
